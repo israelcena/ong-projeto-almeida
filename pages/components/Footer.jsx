99263d30895a46a27991fe1b32a1fca8d87594bb
israelcena <israelsjm@gmail.com>
2022-06-14 20:44:06 -0300

    fix spacing

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -8,55 +8,52 @@ import { BsLinkedin } from "react-icons/bs";
 
 export default function Footer() {
 	return (
-		<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-			<div className="col-md-4 d-flex align-items-center">
-				<Link href="/">
-					<a className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-						<BsInfoCircleFill />
-					</a>
-				</Link>
-				<span className="mb-3 mb-md-0 text-muted">
-					© 2022 Projeto Almeida, Organização sem fins lucrativos
-				</span>
-			</div>
+		<footer className="pb-3 mb-4 border-top">
+			<div className="container d-flex flex-wrap justify-content-between align-items-center">
+				<div className="col-md-4 d-flex align-items-center">
+					<span className="mb-3 mb-md-0 text-muted">
+						© 2022 Projeto Almeida, Organização sem fins lucrativos
+					</span>
+				</div>
 
-			<ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-				<li className="ms-3">
-					<Link href="/">
-						<a className="text-muted">
-							<BsFacebook />
-						</a>
-					</Link>
-				</li>
-				<li className="ms-3">
-					<Link href="/">
-						<a className="text-muted">
-							<BsInstagram />
-						</a>
-					</Link>
-				</li>
-				<li className="ms-3">
-					<Link href="/">
-						<a className="text-muted">
-							<BsYoutube />
-						</a>
-					</Link>
-				</li>
-				<li className="ms-3">
-					<Link href="/">
-						<a className="text-muted">
-							<BsWhatsapp />
-						</a>
-					</Link>
-				</li>
-				<li className="ms-3">
-					<Link href="/">
-						<a className="text-muted">
-							<BsLinkedin />
-						</a>
-					</Link>
-				</li>
-			</ul>
+				<ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+					<li className="ms-3">
+						<Link href="/">
+							<a className="text-muted">
+								<BsFacebook />
+							</a>
+						</Link>
+					</li>
+					<li className="ms-3">
+						<Link href="/">
+							<a className="text-muted">
+								<BsInstagram />
+							</a>
+						</Link>
+					</li>
+					<li className="ms-3">
+						<Link href="/">
+							<a className="text-muted">
+								<BsYoutube />
+							</a>
+						</Link>
+					</li>
+					<li className="ms-3">
+						<Link href="/">
+							<a className="text-muted">
+								<BsWhatsapp />
+							</a>
+						</Link>
+					</li>
+					<li className="ms-3">
+						<Link href="/">
+							<a className="text-muted">
+								<BsLinkedin />
+							</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</footer>
 	);
 }
