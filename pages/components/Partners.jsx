3a6cc2c51a0cc8b6bb926e1fca78279657180e2f
israelcena <israelsjm@gmail.com>
2022-06-14 20:39:33 -0300

    add partners

--- pages/components/Partners.jsx
+++ pages/components/Partners.jsx
@@ -1,5 +1,224 @@
+import { BsArrowRight } from "react-icons/bs";
+
 export default function Partners() {
-  return (
-    <section>Partners</section>
-  )
+	return (
+		<section
+			className="my-5 py-5 text-center"
+			style={{
+				backgroundColor: "#74b9ff",
+			}}
+		>
+			<div className="container">
+				<h1 className="text-primary mb-3">
+					Parceiros
+					<br />
+					<span>
+						<BsArrowRight />
+					</span>
+				</h1>
+				<div>
+					<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+						<div className="col">
+							<div className="card shadow-sm">
+								<svg
+									className="bd-placeholder-img card-img-top"
+									width="100%"
+									height="225"
+									xmlns="http://www.w3.org/2000/svg"
+									role="img"
+									aria-label="Placeholder: Thumbnail"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+								>
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#55595c"></rect>
+								</svg>
+								<div className="card-body">
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural
+										lead-in to additional content. This content is a little bit
+										longer.
+									</p>
+									<div>
+										<button
+											type="button"
+											className="btn btn-sm btn-outline-secondary"
+										>
+											Visitar Website
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col">
+							<div className="card shadow-sm">
+								<svg
+									className="bd-placeholder-img card-img-top"
+									width="100%"
+									height="225"
+									xmlns="http://www.w3.org/2000/svg"
+									role="img"
+									aria-label="Placeholder: Thumbnail"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+								>
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#55595c"></rect>
+								</svg>
+
+								<div className="card-body">
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural
+										lead-in to additional content. This content is a little bit
+										longer.
+									</p>
+									<div>
+										<button
+											type="button"
+											className="btn btn-sm btn-outline-secondary"
+										>
+											Visitar Website
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col">
+							<div className="card shadow-sm">
+								<svg
+									className="bd-placeholder-img card-img-top"
+									width="100%"
+									height="225"
+									xmlns="http://www.w3.org/2000/svg"
+									role="img"
+									aria-label="Placeholder: Thumbnail"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+								>
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#55595c"></rect>
+								</svg>
+
+								<div className="card-body">
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural
+										lead-in to additional content. This content is a little bit
+										longer.
+									</p>
+									<div>
+										<button
+											type="button"
+											className="btn btn-sm btn-outline-secondary"
+										>
+											Visitar Website
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col">
+							<div className="card shadow-sm">
+								<svg
+									className="bd-placeholder-img card-img-top"
+									width="100%"
+									height="225"
+									xmlns="http://www.w3.org/2000/svg"
+									role="img"
+									aria-label="Placeholder: Thumbnail"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+								>
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#55595c"></rect>
+								</svg>
+
+								<div className="card-body">
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural
+										lead-in to additional content. This content is a little bit
+										longer.
+									</p>
+									<div>
+										<button
+											type="button"
+											className="btn btn-sm btn-outline-secondary"
+										>
+											Visitar Website
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col">
+							<div className="card shadow-sm">
+								<svg
+									className="bd-placeholder-img card-img-top"
+									width="100%"
+									height="225"
+									xmlns="http://www.w3.org/2000/svg"
+									role="img"
+									aria-label="Placeholder: Thumbnail"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+								>
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#55595c"></rect>
+								</svg>
+
+								<div className="card-body">
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural
+										lead-in to additional content. This content is a little bit
+										longer.
+									</p>
+									<div className="d-fle m-autox justify-content-between align-items-center">
+										<button
+											type="button"
+											className="btn btn-sm btn-outline-secondary"
+										>
+											Visitar Website
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col">
+							<div className="card shadow-sm">
+								<svg
+									className="bd-placeholder-img card-img-top"
+									width="100%"
+									height="225"
+									xmlns="http://www.w3.org/2000/svg"
+									role="img"
+									aria-label="Placeholder: Thumbnail"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+								>
+									<title>Placeholder</title>
+									<rect width="100%" height="100%" fill="#55595c"></rect>
+								</svg>
+
+								<div className="card-body">
+									<p className="card-text">
+										This is a wider card with supporting text below as a natural
+										lead-in to additional content. This content is a little bit
+										longer.
+									</p>
+									<div>
+										<button
+											type="button"
+											className="btn btn-sm btn-outline-secondary"
+										>
+											Visitar Website
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
