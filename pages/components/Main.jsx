ba6c844e403dd692a0b8020316a47b3ec32d8879
israelcena <israelsjm@gmail.com>
2022-06-14 18:39:02 -0300

    rework main component

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -1,18 +1,15 @@
 export default function Main() {
 	return (
-		<main className="mt-4">
-			<div className="px-4 py-5 my-5 text-center">
-				<h1 className="display-5 fw-bold">Projeto Almeida</h1>
-				<div className="col-lg-6 mx-auto">
-					<p className="lead mb-4">de Volta à sociedade</p>
+		<main>
+			<div className="px-4 py-5 text-center bg-primary text-white">
+				<h1 className="display-3 fw-bold text-uppercase">Projeto Almeida</h1>
+				<div className="col-lg-6 mx-auto ">
+					<p className="lead mb-4 fs-1">de Volta à sociedade</p>
 					<div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-						<button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+						<button type="button" className="btn btn-success btn-lg px-4 gap-3">
 							Faça um Doação
 						</button>
-						<button
-							type="button"
-							className="btn btn-outline-secondary btn-lg px-4"
-						>
+						<button type="button" className="btn btn-secondary btn-lg px-4">
 							Conheça
 						</button>
 					</div>
