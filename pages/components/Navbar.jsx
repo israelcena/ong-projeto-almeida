12923f9f690235f7d2c2c905f14b33b522bdf31b
israelcena <israelsjm@gmail.com>
2022-06-14 18:16:02 -0300

    include navbar Logo

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,9 +1,65 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Navbar() {
-return (
-  <nav>
-    NavBar
-    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-  </nav>
-)} 
+	return (
+		<div className="container-xl">
+			<nav
+				className="navbar navbar-expand-lg navbar-light bg-light sticky-top"
+				aria-label="navbar"
+			>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbar"
+					aria-controls="navbar"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbar">
+					<ul className="navbar-nav mb-2 mb-lg-0">
+						<div className="d-flex">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#">
+									Nossos Projetos
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Quem Somos
+								</a>
+							</li>
+						</div>
+
+						<div>
+							<li>
+								<a className="navbar-brand" href="/">
+									<Image
+										src="/images/logoProjetoAlmeida.jpeg"
+										alt="Logo"
+										width={100}
+										height={100}
+									/>
+								</a>
+							</li>
+						</div>
+						<div className="d-flex">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#">
+									Nossos Projetos
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#">
+									Quem Somos
+								</a>
+							</li>
+						</div>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
+}
