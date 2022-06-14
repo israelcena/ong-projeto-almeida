c2e7d48601ba4a7cc7529af228da38a2aeaafa1e
israelcena <israelsjm@gmail.com>
2022-06-14 18:06:30 -0300

    update main project

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -1,5 +1,23 @@
 export default function Main() {
-  return (
-    <main>Main</main>
-  )
+	return (
+		<main className="mt-4">
+			<div className="px-4 py-5 my-5 text-center">
+				<h1 className="display-5 fw-bold">Projeto Almeida</h1>
+				<div className="col-lg-6 mx-auto">
+					<p className="lead mb-4">de Volta à sociedade</p>
+					<div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+						<button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+							Faça um Doação
+						</button>
+						<button
+							type="button"
+							className="btn btn-outline-secondary btn-lg px-4"
+						>
+							Conheça
+						</button>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
